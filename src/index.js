426f1f0e167a04795f9c643c34175fa4a62ec6a3
Rudy <rudy.quinternet@gmail.com>
2021-03-26 17:09:55 +0100

    add libs

--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Navbar from './Navbar';
-import reportWebVitals from './reportWebVitals';
-
-
-function renderNavbar(argument) {
-	ReactDOM.render(
-	    <Navbar />,
-	  document.getElementById('navbar')
-	);
-}
+// import reportWebVitals from './reportWebVitals';
 
 function renderApp(argument) {
 	ReactDOM.render(
@@ -24,5 +15,4 @@ function renderApp(argument) {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
-renderNavbar();
 renderApp();
