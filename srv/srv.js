6f4e36e0a87dd2716ed86b713b7351489bdaff25
Rudy <rudy.quinternet@gmail.com>
2021-03-31 21:37:53 +0200

    add refresh to srv side

--- srv/srv.js
+++ srv/srv.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 
 var redirect_uri = 'http://localhost:3000'
@@ -14,26 +14,49 @@ var client_id = '06b399096b834aa4889d88263fe2d969'
 var client_secret = 'ce9ff0594b9d4d0f9247bde6b525e6b6'
 
 app.post("/login", (req, res) => {
-  const code = req.body.code
-
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: redirect_uri,
-    clientId: client_id,
-    clientSecret: client_secret,
-  })
+    const code = req.body.code
+    
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: redirect_uri,
+        clientId: client_id,
+        clientSecret: client_secret,
+    })
     
     spotifyApi
     .authorizationCodeGrant(code)
     .then(data => {
-      res.json({
-        accessToken: data.body.access_token,
-        refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
-      })
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in,
+        })
     })
     .catch(err => {
-      res.sendStatus(400)
-      console.log(err)
+        res.sendStatus(400)
+        console.log(err)
+    })
+})
+
+app.post("/refresh", (req, res) => {
+    
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: redirect_uri,
+        clientId: client_id,
+        clientSecret: client_secret,
+        refreshToken : req.body.refreshToken,
+    })
+    
+    spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+        res.json({
+            accessToken: data.body.accessToken,
+            expiresIn: data.body.expiresIn,
+        })
+    })
+    .catch(err => {
+        res.sendStatus(400)
+        console.log(err)
     })
 })
 
